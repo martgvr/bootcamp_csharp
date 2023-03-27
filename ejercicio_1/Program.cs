d2ce27089ec8eb04c350dd238307a7b6c1d3fbc8
Martin Guevara <martin_91@live.com.ar>
2023-03-27 00:34:17 -0300

    Ejercicios sesion 1

---
+++ ejercicio_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ejercicio 1
+
+Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es " + nombre);
+
+// Ejercicio 2
+
+DateTime hoy = DateTime.Now;
+Console.WriteLine("Hoy es: " + hoy.ToString());
